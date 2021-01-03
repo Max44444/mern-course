b08781586c68d7dec4210d43cf9045e1f2d709f0
Max44444 <Skorokhodov.m.v@ukr.net>
2021-01-03 20:50:58 +0200

    authorization on client + navigation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -21,7 +21,7 @@ router.post(
       if (!errors.isEmpty()) {
         return res.status(400).json({
           errors: errors.array(),
-          message: 'Incorrect data during registrationa'
+          message: 'Incorrect data during registration'
         })
       }
 
